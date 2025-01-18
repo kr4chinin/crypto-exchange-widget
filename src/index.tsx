8ffefc47ada3311ef8a4922e41f969f5d75e4d78
Ilya Kruchinin <kr4chinin@mail.ru>
2025-01-18 21:38:54 +0800

    chore: remove boilerplate react-router-dom from the project

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,18 @@ import { MantineProvider } from '@mantine/core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
-import { RouterProvider } from 'react-router-dom';
-import { createRouter } from './config/createRouter';
+import { App } from './App';
 
 export const queryClient = new QueryClient();
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
-const router = createRouter();
-
 root.render(
 	<ErrorBoundary fallback={<div>Something went wrong!</div>}>
 		<QueryClientProvider client={queryClient}>
 			<MantineProvider defaultColorScheme="dark">
-				<RouterProvider router={router} />
+				<App />
 			</MantineProvider>
 		</QueryClientProvider>
 	</ErrorBoundary>
