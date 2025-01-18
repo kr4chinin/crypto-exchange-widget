8a97e8ef61b684088279076e0e3dba3983412241
Ilya Kruchinin <kr4chinin@mail.ru>
2025-01-18 22:29:34 +0800

    style: small square decorative bottom gradient for CryptoExchange, change Exchange button gradient

--- src/widgets/CryptoExchange.tsx
+++ src/widgets/CryptoExchange.tsx
@@ -17,13 +17,45 @@ const Root = styled.div`
 
 	padding: 16px 24px;
 	border-radius: 16px;
-	border: 1px solid var(--color-neutral-950);
 	backdrop-filter: blur(8px);
+	border: 1px solid var(--color-neutral-950);
 	box-shadow:
 		0 4px 12px rgba(255, 255, 255, 0.05),
 		0 6px 16px -4px rgba(255, 255, 255, 0.06),
 		inset 0 0 0 1px rgba(255, 255, 255, 0.085);
 	background: color-mix(in srgb, var(--color-neutral-800) 85%, transparent);
+
+	&::after {
+		content: '';
+
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+
+		height: 32px;
+
+		background-image: linear-gradient(
+				45deg,
+				rgba(255, 255, 255, 0.03) 25%,
+				transparent 25%,
+				transparent 75%,
+				rgba(255, 255, 255, 0.03) 75%
+			),
+			linear-gradient(
+				45deg,
+				rgba(255, 255, 255, 0.03) 25%,
+				transparent 25%,
+				transparent 75%,
+				rgba(255, 255, 255, 0.03) 75%
+			);
+		background-size: 10px 10px;
+		background-position:
+			0 0,
+			5px 5px;
+		border-radius: 0 0 16px 16px;
+		opacity: 0.8;
+	}
 `;
 
 const ReverseButtonWrapper = styled.div`
@@ -55,7 +87,11 @@ const CryptoExchange = () => {
 			<BottomBlockWrapper>
 				<Text>1 BTC = 0.001 ETH</Text>
 
-				<Button variant="gradient" w="fit-content">
+				<Button
+					variant="gradient"
+					w="fit-content"
+					gradient={{ from: 'gray', to: 'cyan', deg: 156 }}
+				>
 					Exchange
 				</Button>
 			</BottomBlockWrapper>
