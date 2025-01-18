d7a95620c00153d3b9bc231aa079bb75865aea50
Ilya Kruchinin <kr4chinin@mail.ru>
2025-01-18 22:12:16 +0800

    feat: ReverseButton tooltip

--- src/components/ReverseButton.tsx
+++ src/components/ReverseButton.tsx
@@ -1,12 +1,14 @@
-import { Button, type ButtonProps } from '@mantine/core';
+import { Button, Tooltip, type ButtonProps } from '@mantine/core';
 import { ArrowRightLeft } from 'lucide-react';
 import { memo } from 'react';
 
 const ReverseButton = memo((props: ButtonProps) => {
 	return (
-		<Button variant="default" {...props} w="fit-content">
-			<ArrowRightLeft size={16} />
-		</Button>
+		<Tooltip label="Reverse">
+			<Button variant="default" {...props} w="fit-content">
+				<ArrowRightLeft size={16} />
+			</Button>
+		</Tooltip>
 	);
 });
 
