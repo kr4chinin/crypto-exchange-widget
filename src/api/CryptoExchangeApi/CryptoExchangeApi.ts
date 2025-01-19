0cc9ef09de01a9aa4aca04ea49b1871536bb3f93
Ilya Kruchinin <kr4chinin@mail.ru>
2025-01-19 23:56:07 +0800

    feat: api conversion route

--- src/api/CryptoExchangeApi/CryptoExchangeApi.ts
+++ src/api/CryptoExchangeApi/CryptoExchangeApi.ts
@@ -1,6 +1,7 @@
 import type { AxiosInstance } from 'axios';
 import axios from 'axios';
 import type { CmcCoin } from '~/models/CmcCoin';
+import type { ConversionResponse } from '~/models/ConversionResponse';
 import { ApiRoutes } from '../ApiRoutes';
 
 class CryptoExchangeApi {
@@ -10,6 +11,32 @@ class CryptoExchangeApi {
 
 	getCoins = async (): Promise<CmcCoin[]> => {
 		const response = await this._axios.get(ApiRoutes.COINS);
+
+		return response.data;
+	};
+
+	// Get conversion rate and estimated amount between two coins.
+	// Example request: {{baseApiUrl}}/api/conversion-rate?from=1&to=1027&fromAmount=3
+	// Specify either fromAmount or toAmount, not both.
+	getConversion = async ({
+		from,
+		to,
+		fromAmount,
+		toAmount,
+	}: {
+		from: number;
+		to: number;
+		fromAmount?: number;
+		toAmount?: number;
+	}): Promise<ConversionResponse> => {
+		const response = await this._axios.get(ApiRoutes.CONVERSION, {
+			params: {
+				from,
+				to,
+				fromAmount,
+				toAmount,
+			},
+		});
 
 		return response.data;
 	};
---
+++ src/models/ConversionResponse.ts
@@ -0,0 +1,4 @@
+export interface ConversionResponse {
+	rate: number;
+	estimatedAmount: number;
+}
