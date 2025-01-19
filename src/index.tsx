563d6e06691a2c895f19a6a180af7b6595e79269
Ilya Kruchinin <kr4chinin@mail.ru>
2025-01-19 23:27:33 +0800

    feat: component for error fallback, integrate into ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
 import { App } from './App';
+import { ErrorFallback } from './components/ErrorFallback';
 
 export const queryClient = new QueryClient();
 
@@ -12,11 +13,11 @@ const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
-	<ErrorBoundary fallback={<div>Something went wrong!</div>}>
-		<QueryClientProvider client={queryClient}>
-			<MantineProvider defaultColorScheme="dark">
+	<QueryClientProvider client={queryClient}>
+		<MantineProvider defaultColorScheme="dark">
+			<ErrorBoundary fallback={<ErrorFallback />}>
 				<App />
-			</MantineProvider>
-		</QueryClientProvider>
-	</ErrorBoundary>
+			</ErrorBoundary>
+		</MantineProvider>
+	</QueryClientProvider>
 );
